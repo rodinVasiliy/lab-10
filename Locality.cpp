1c1b47ac1f49b7784052d8592a5b963bfe10af67
rodinVasiliy <57068440+rodinVasiliy@users.noreply.github.com>
2020-05-17 21:39:43 +0300

    Update Locality.cpp

--- Locality.cpp
+++ Locality.cpp
@@ -3,30 +3,36 @@
 //
 #include <iostream>
 #include "Locality.h"
-char* Locality::getName() const {
+
+char *Locality::getName() const {
     return _name;
 }
+
 double Locality::getnumberOfInhabitants() {
     return _numberOfInhabitants;
 }
+
 double Locality::getnumberOfInhabitants() const {
     return _numberOfInhabitants;
 }
-void Locality::Print() const {
-    std::cout<<_name<<" ";
+
+void Print(const Locality &rhs) {
+    std::cout << rhs.getName() << " ";
 }
 
-std::ostream& operator <<(std::ostream &out, const Locality &rhs) {
-    rhs.Print();
-    return out<<rhs.getnumberOfInhabitants();
+std::ostream &operator<<(std::ostream &out, const Locality &rhs) {
+    Print(rhs);
+    return out << rhs.getnumberOfInhabitants();
 }
-bool operator ==(const Locality& lhs,const Locality& rhs){
-    if(lhs.getnumberOfInhabitants()!=rhs.getnumberOfInhabitants())return false;
-    if(strlen(lhs.getName())!=strlen(rhs.getName()))return false;
-    char* lhsStr=lhs.getName();
-    char* rhsStr=rhs.getName();
-    for(std::size_t i=0;i<strlen(lhs.getName());++i){
-        if(lhsStr[i]!=rhsStr[i])return false;
+
+bool operator==(const Locality &lhs, const Locality &rhs) {
+    if (lhs.getnumberOfInhabitants() != rhs.getnumberOfInhabitants())return false;
+    if (strlen(lhs.getName()) != strlen(rhs.getName()))return false;
+    char *lhsStr = lhs.getName();
+    char *rhsStr = rhs.getName();
+    for (std::size_t i = 0; i < strlen(lhs.getName()); ++i) {
+        if (lhsStr[i] != rhsStr[i])return false;
     }
     return true;
 }
+
